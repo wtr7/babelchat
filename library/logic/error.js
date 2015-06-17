18f4190e58dbb35fc51c569d8aae4be48c2560ea
wtr77 <wouter_vanrespaille@hotmail.com>
2015-06-17 11:54:41 +0200

    Remove redundant files

--- library/logic/error.js
+++
@@ -1,8 +0,0 @@
-// handle errors
-handleError = function handleError(err, res) {
-	var message = (err && err.message) ? err : {message: 'Internal Server Error', status: 'internalServerError', code: 500};
-
-	res.status(message.code).json({
-		error: message
-	});
-}
